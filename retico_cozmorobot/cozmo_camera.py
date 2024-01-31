a2be35fb985a6e294338a891f16285c14069f333
catherinehenry <catherinemshenry@gmail.com>
2024-01-30 20:25:25 -0700

    update v2 cozmo camera module to take iu input

--- retico_cozmorobot/cozmo_camera.py
+++ retico_cozmorobot/cozmo_camera.py
@@ -4,8 +4,11 @@ import time
 import asyncio
 import sys
 
+import cv2
+
 # retico
 import retico_core
+from retico_core.robot import IACMotorAction
 from retico_vision.vision import ImageIU
 
 # cozmo
@@ -23,7 +26,7 @@ import base64
 
 
 
-class CozmoCameraModule(retico_core.AbstractProducingModule):
+class CozmoCameraModule(retico_core.AbstractModule):
     '''
     use_viewer=True must be set in cozmo.run_program
     '''
@@ -37,43 +40,74 @@ class CozmoCameraModule(retico_core.AbstractProducingModule):
         return "A module that tracks cozmo camera frames."
 
     @staticmethod
+    def input_ius():
+        return [IACMotorAction]
+
+    @staticmethod
     def output_iu():
         return ImageIU
+
 
     def __init__(self, robot, exposure=0.05, gain=0.1, **kwargs):
         # for exp room:exposure=0.05, gain=0.05
         super().__init__(**kwargs)
         self.robot = robot
-        self.robot.move_lift(5)
+        # My cozmo isn't reliably able to lift the lift up so set down to 0 for now. - Catherine (its not obstructing the camera so its fine)
+        self.robot.set_lift_height(0.0, in_parallel=True).wait_for_completed() # this doesnt work for lifting  up for some reason
+        # self.robot.move_lift(5) # this locks the track cannot do other stuff later
+        # use this to lift the lift because it can be stopped later. set lift height doesnt work for some reason.
+        # self.robot.play_anim_trigger(cozmo.anim.Triggers.CubePounceIdleLiftUp).wait_for_completed()        self.exposure_amount = exposure
         self.exposure_amount = exposure
         self.gain_amount = gain
         self.img_queue = deque(maxlen=1)
-
+        self.queue = deque()
 
         # NOTE: was seeing intermittent issues when this was in setup -- the exposure/gain was not setting correctly and would be too bright
         # self.configure_camera()
 
+    def process_update(self, update_message):
 
-    def process_update(self, blank):
+        for iu, ut in update_message:
+            if ut != retico_core.UpdateType.ADD:
+                continue
+            else:
+                self.queue.append(iu)
 
-        if len(self.img_queue) > 0:
+        # if len(self.img_queue) > 0:
+        #     img = self.img_queue.popleft()
+        #     # img = np.array(img)
+        #     output_iu = self.create_iu(None)
+        #     output_iu.set_image(img, 1, 1)
+        #     return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+
+
+            # bytes = img.tobytes()
+            # output_iu.set_image(base64.b64encode(bytes).decode(), 1, 1)
+            # return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+
+    def _extractor_thread(self):
+        while self._extractor_thread_active:
+            if len(self.queue) == 0:
+                time.sleep(0.5)
+                continue
+
+            input_iu = self.queue.popleft()
             img = self.img_queue.popleft()
             # img = np.array(img)
-            output_iu = self.create_iu(None)
-            bytes = img.tobytes()
-            output_iu.set_image(base64.b64encode(bytes).decode(), 1, 1)
-            return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+            output_iu = self.create_iu(input_iu)
+            img = np.array(img)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(img)
+            output_iu.set_image(img, 1, 1)
+            output_iu.set_flow_uuid(input_iu.flow_uuid)
+            output_iu.set_execution_uuid(input_iu.execution_uuid)
+            output_iu.set_motor_action(input_iu.motor_action)
+            self.robot.camera.image_stream_enabled = False
+            um = retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+            self.append(um)
 
-    
-    # def process_iu(self, input_iu):
-    #     if len(self.img_queue) > 0:
-    #         img = self.img_queue.popleft()
-    #         # img = np.array(img)
-    #         output_iu = self.create_iu(input_iu)
-    #         output_iu.set_image(img, 1, 1)
-    #         return output_iu
-
-    #     return None
+        #     return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
+        return None
 
     def configure_camera(self):
         self.robot.camera.color_image_enabled = True
@@ -88,13 +122,17 @@ class CozmoCameraModule(retico_core.AbstractProducingModule):
         actual_gain = (1-self.gain_amount)*min_gain + self.gain_amount*max_gain
         self.robot.camera.set_manual_exposure(exposure_time,actual_gain)
 
-    def setup(self):
-        
+    def prepare_run(self):
         def handle_image(evt, obj=None, tap_count=None,  **kwargs):
             self.img_queue.append(evt.image)
 
         self.configure_camera()
+        time.sleep(20)
         self.robot.world.add_event_handler(cozmo.camera.EvtNewRawCameraImage, handle_image)
 
+        self._extractor_thread_active = True
+        threading.Thread(target=self._extractor_thread).start()
 
+    def shutdown(self):
+        self._extractor_thread_active = False
 
