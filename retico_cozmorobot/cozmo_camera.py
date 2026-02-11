b2c6bde87ad3208affa74bc7517900368a1bb09e
catherinehenry <catherinemshenry@gmail.com>
2026-02-11 11:41:31 -0700

    remove old code from cozmo camera

--- retico_cozmorobot/cozmo_camera.py
+++ retico_cozmorobot/cozmo_camera.py
@@ -1,29 +1,18 @@
-import functools
-import threading
-import time
-import asyncio
+import os
+# cozmo
 import sys
-
-import cv2
+import threading
 
 # retico
 import retico_core
 from retico_core.robot import IACMotorAction
 from retico_vision.vision import ImageIU
 
-# cozmo
-import sys
-import os
 sys.path.append(os.environ['COZMO'])
 import cozmo
-# import cv2
 import time
 
 from collections import deque
-import numpy as np
-from PIL import Image
-import base64
-
 
 
 class CozmoCameraModule(retico_core.AbstractModule):
@@ -49,14 +38,9 @@ class CozmoCameraModule(retico_core.AbstractModule):
 
 
     def __init__(self, robot, exposure=40, gain=2, **kwargs):
-        # for exp room:exposure=0.05, gain=0.05
         super().__init__(**kwargs)
         self.robot = robot
-        # My cozmo isn't reliably able to lift the lift up so set down to 0 for now. - Catherine (its not obstructing the camera so its fine)
-        self.robot.set_lift_height(0.0, in_parallel=True).wait_for_completed() # this doesnt work for lifting  up for some reason
-        # self.robot.move_lift(5) # this locks the track cannot do other stuff later
-        # use this to lift the lift because it can be stopped later. set lift height doesnt work for some reason.
-        # self.robot.play_anim_trigger(cozmo.anim.Triggers.CubePounceIdleLiftUp).wait_for_completed()        self.exposure_amount = exposure
+        self.robot.set_lift_height(0.0, in_parallel=True).wait_for_completed()
         self.exposure_amount = exposure
         self.gain_amount = gain
         self.configure_camera()
@@ -65,9 +49,6 @@ class CozmoCameraModule(retico_core.AbstractModule):
         self.queue = deque()
 
 
-    # NOTE: was seeing intermittent issues when this was in setup -- the exposure/gain was not setting correctly and would be too bright
-        # self.configure_camera()
-
     def process_update(self, update_message):
 
         for iu, ut in update_message:
@@ -75,18 +56,6 @@ class CozmoCameraModule(retico_core.AbstractModule):
                 continue
             else:
                 self.queue.append(iu)
-
-        # if len(self.img_queue) > 0:
-        #     img = self.img_queue.popleft()
-        #     # img = np.array(img)
-        #     output_iu = self.create_iu(None)
-        #     output_iu.set_image(img, 1, 1)
-        #     return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
-
-
-            # bytes = img.tobytes()
-            # output_iu.set_image(base64.b64encode(bytes).decode(), 1, 1)
-            # return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
 
     def _extractor_thread(self):
         while self._extractor_thread_active:
@@ -98,11 +67,7 @@ class CozmoCameraModule(retico_core.AbstractModule):
             while len(self.img_queue) < 1:
                 time.sleep(0.05)
             img = self.img_queue.popleft()
-            # img = np.array(img)
             output_iu = self.create_iu(input_iu)
-            # img = np.array(img)
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            # img = Image.fromarray(img)
             output_iu.set_image(img, 1, 1)
             output_iu.set_flow_uuid(input_iu.flow_uuid)
             output_iu.set_execution_uuid(input_iu.execution_uuid)
@@ -110,8 +75,6 @@ class CozmoCameraModule(retico_core.AbstractModule):
             self.robot.camera.image_stream_enabled = False
             um = retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
             self.append(um)
-
-        #     return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
         return None
 
     def configure_camera(self):
@@ -123,14 +86,12 @@ class CozmoCameraModule(retico_core.AbstractModule):
         min_exposure = self.robot.camera.config.min_exposure_time_ms
         max_exposure = self.robot.camera.config.max_exposure_time_ms
         trimmed_exposure = max(min_exposure, min(self.exposure_amount, max_exposure))
-        # exposure_time = (1 - self.exposure_amount) * min_exposure + self.exposure_amount * max_exposure
         # Lerp gain
         min_gain = self.robot.camera.config.min_gain
         max_gain = self.robot.camera.config.max_gain
         trimmed_gain =  max(min_gain, min(self.gain_amount, max_gain))
         print(f"[Before Setting] Exposure: {self.robot.camera.exposure_ms}, Gain: {self.robot.camera.gain}")
 
-        # actual_gain = (1-self.gain_amount)*min_gain + self.gain_amount*max_gain
         self.robot.camera.set_manual_exposure(trimmed_exposure,trimmed_gain)
         time.sleep(5) # wait for these settings to propagate through to Cozmo
         # Setting twice, I've found that sometimes the first set doesn't reliably apply
